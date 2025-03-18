c6897fb264528048a5a0c33994b44773cb4e88a9
Hadi Hz <hijazih24@gmail.com>
2025-03-18 15:13:56 +0200

    Create Record Model

---
+++ database/migrations/2025_03_17_195228_create_records_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->id(); // record id
+            $table->json('data'); // record data, stored as JSON (e.g. {"name": "John", "age": 30})
+            // foreign id for the template type
+            $table->timestamps(); // created_at and updated_at columns
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('records');
+    }
+};
