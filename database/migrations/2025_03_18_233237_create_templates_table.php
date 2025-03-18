0f09b7b7a1bf993fd52cc2aac6f1aefe9facc944
Hadi Hz <hijazih24@gmail.com>
2025-03-19 01:36:44 +0200

    Create Template Model, Change Record schema

--- database/migrations/2025_03_18_233237_create_templates_table.php
+++ database/migrations/2025_03_18_233237_create_templates_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Template;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('records', function (Blueprint $table) {
+        Schema::create('templates', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Template::class);
+            $table->string('name');
+            $table->json('description')->nullable();
             $table->timestamps();
         });
     }
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('records');
+        Schema::dropIfExists('templates');
     }
 };
