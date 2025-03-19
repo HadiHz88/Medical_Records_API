7a7f692f40814a3a0f512ba15be72ae9cd176c9c
jadchahine111 <jadalichahine@gmail.com>
2025-03-19 02:20:53 +0200

    Setup tailwindcss

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
     <title>React inside Laravel</title>
     @viteReactRefresh
     @vite('resources/js/app.jsx') <!-- This will load your React JS code -->
+    @vite('resources/css/app.css')
 </head>
 <body>
     <div id="app"></div> <!-- React will mount here -->
